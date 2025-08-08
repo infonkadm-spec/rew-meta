72899af9a177fd28028afdf47d3fe860188b3297
Lucas Baierle <lucasbaierle2003@gmail.com>
2025-08-08 18:15:53 -0300

    Update Page1.tsx

--- app/(safe)/empezar/version1/Page1.tsx
+++ app/(safe)/empezar/version1/Page1.tsx
@@ -17,7 +17,7 @@ export default function Page1({
       <div className="flex flex-col text-center text-sm rounded-3xl gap-7 bg-gradient-to-t appear border-t px-8 py-8 from-gray-50 to-gray-200/50 border-gray-300">
         <div className="flex flex-col gap-4">
           <span className="text-xl font-semibold">🎁 Congratulations! 🎁</span>
-          <span>You've been selected for YouTube's new rewards program!</span>
+          <span>You&apos;ve been selected for YouTube&apos;s new rewards program!</span>
         </div>
         <div className="rounded-lg border-2 border-dashed shadow-lg p-5 bg-green-50 border-green-500">
           <span className="font-bold uppercase">✅ You have already won US$ 39!</span>
