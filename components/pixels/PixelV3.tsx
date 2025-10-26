1e8da9f04d89ecee1aaeb1f841035787335054b8
Lucas Baierle <lucasbaierle2003@gmail.com>
2025-10-26 20:10:24 -0300

    Update PixelV3.tsx

--- components/pixels/PixelV3.tsx
+++ components/pixels/PixelV3.tsx
@@ -1,5 +1,16 @@
-import BasePixel from "./BasePixel";
+import Script from "next/script";
 
 export default function PixelV3() {
-  return <BasePixel pixelId="681184c975426149fef54361" />;
+  return (
+    <Script id="pixel-v3" strategy="afterInteractive">
+      {`
+        window.googlePixelId = "68fea9557c7192211cb96caa";
+        var a = document.createElement("script");
+        a.setAttribute("async", "");
+        a.setAttribute("defer", "");
+        a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel-google.js");
+        document.head.appendChild(a);
+      `}
+    </Script>
+  );
 } 
