d2d985311f884053c27d8cf412ca60580b698b86
Lucas Baierle <lucasbaierle2003@gmail.com>
2025-10-27 14:50:37 -0300

    inversão /start e /app

--- app/(safe)/start/page.tsx
+++ app/(safe)/start/page.tsx
@@ -70,15 +70,17 @@ export default function Page() {
 
   return (
     <div className="flex flex-col w-full max-w-xl gap-5 px-4 py-5 pb-10">
-      <div className="flex justify-between items-center">
-        <Logo />
-        <Balance page={page} />
-      </div>
+      {page !== 1 && (
+        <div className="flex justify-between items-center">
+          <Logo />
+          <Balance page={page} />
+        </div>
+      )}
       <PageContent
         active={active}
         handleClick={handleClick}
       />
-      {openModal && <Modal value={rewardValues[page] || 58} />}
+      {openModal && <Modal value={rewardValues[page] || 47} />}
       {isInfoPage && (
         <div className="flex flex-col justify-center text-center gap-3 p-4 text-gray-400/70">
           <span className="text-sm">© 2025 YouTube Rewards</span>
@@ -87,5 +89,4 @@ export default function Page() {
       )}
     </div>
   );
-
 };
